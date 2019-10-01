d13862810fd72662d54a4fab705565b12b21318e
Arthur Puccinelli <arthur.puccinelli@lumetec.com.br>
2019-10-01 20:41:23 -0300

    Criação sub pasta Arthur

---
+++ Arthur Puccinelli - AV GitHub/book/target/generated-sources/annotations/tads_tdw/entities/Book_.java
@@ -0,0 +1,18 @@
+package tads_tdw.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-27T15:32:17")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, Integer> year;
+    public static volatile SingularAttribute<Book, String> author;
+    public static volatile SingularAttribute<Book, Float> price;
+    public static volatile SingularAttribute<Book, String> name;
+    public static volatile SingularAttribute<Book, Integer> id;
+    public static volatile SingularAttribute<Book, String> category;
+
+}
